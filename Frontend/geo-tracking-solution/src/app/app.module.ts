0694954a3d78c248f33dd1253d71d76f6e6ca014
Benjamin Suljevic <bsuljevic1809@gmail.com>
2024-09-09 11:33:52 +0200

    new map component

--- Frontend/geo-tracking-solution/src/app/app.module.ts
+++ Frontend/geo-tracking-solution/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { ConfigurationBarComponent } from './configuration-bar/configuration-bar.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationBarComponent,
-    ConfigurationBarComponent
+    ConfigurationBarComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
---
+++ Frontend/geo-tracking-solution/src/app/components/map/map.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrl: './map.component.css'
+})
+export class MapComponent {
+
+}
