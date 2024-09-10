349d33717c2d38d3bd64b42badc5cfe8efcfa704
Benjamin Suljevic <bsuljevic1809@gmail.com>
2024-09-10 08:01:00 +0200

    start with navigation bar with dark/light mode switch

--- Frontend/geo-tracking-solution/src/app/app.module.ts
+++ Frontend/geo-tracking-solution/src/app/app.module.ts
@@ -4,8 +4,8 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
-import { ConfigurationBarComponent } from './configuration-bar/configuration-bar.component';
+import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
+import { ConfigurationBarComponent } from './components/configuration-bar/configuration-bar.component';
 
 @NgModule({
   declarations: [
--- Frontend/geo-tracking-solution/src/app/components/navigation-bar/navigation-bar.component.ts
+++ Frontend/geo-tracking-solution/src/app/components/navigation-bar/navigation-bar.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrl: './navigation-bar.component.css'
 })
 export class NavigationBarComponent {
+    isDarkMode = false;
+
+    toggleDarkMode(){
+      this.isDarkMode = !this.isDarkMode;
+      if (this.isDarkMode){
+        document.body.classList.add('dark-mode');
+      }else{
+        document.body.classList.remove('dark-mode')
+      }
+    }
+
 
 }
