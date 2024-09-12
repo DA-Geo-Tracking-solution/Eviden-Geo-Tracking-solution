7eb53b2db6d8fc8457dba7004f6474260a6a0297
Benjamin Suljevic <bsuljevic1809@gmail.com>
2024-09-12 09:30:37 +0200

    changes at the navigation bar

--- Frontend/geo-tracking-solution/src/app/components/navigation-bar/navigation-bar.component.ts
+++ Frontend/geo-tracking-solution/src/app/components/navigation-bar/navigation-bar.component.ts
@@ -3,19 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navigation-bar',
   templateUrl: './navigation-bar.component.html',
-  styleUrl: './navigation-bar.component.css'
+  styleUrls: ['./navigation-bar.component.css']
 })
+
 export class NavigationBarComponent {
-    isDarkMode = false;
+  isDarkMode = false;
 
-    toggleDarkMode(){
-      this.isDarkMode = !this.isDarkMode;
-      if (this.isDarkMode){
-        document.body.classList.add('dark-mode');
-      }else{
-        document.body.classList.remove('dark-mode')
-      }
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+
+    if (this.isDarkMode) {
+      document.body.classList.add('dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
     }
-
-
+  }
 }
