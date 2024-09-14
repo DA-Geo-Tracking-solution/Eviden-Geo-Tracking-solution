1eba046279940c5f767fa9ca57e026642621db6c
Benjamin Suljevic <bsuljevic1809@gmail.com>
2024-09-14 12:22:25 +0200

    new chat style

--- Frontend/geo-tracking-solution/src/app/components/contact-list/contact-list.component.ts
+++ Frontend/geo-tracking-solution/src/app/components/contact-list/contact-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-contact-list',
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrl: './contact-list.component.css'
 })
 export class ContactListComponent {
+
+  @Output() contactSelected = new EventEmitter<string>();
+
+  // TODO: Muss später geändert werden zu den Kontakten in der Datenbank
+  contacts: string[] = ['Kontakt 1', 'Kontakt 2', 'Kontakt 3'];
+
+  selectContact(contact: string):void{
+    this.contactSelected.emit(contact);
+  }
 
 }
