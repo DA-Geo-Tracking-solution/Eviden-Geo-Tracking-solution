02c502debf0d3dd56229db0f2300942b28a18be4
Benjamin Suljevic <bsuljevic1809@gmail.com>
2024-10-04 10:07:43 +0200

    system color mode switch on login page

--- Frontend/geo-tracking-solution/src/app/components/login/login.component.ts
+++ Frontend/geo-tracking-solution/src/app/components/login/login.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  constructor(private renderer: Renderer2) { }
+
+  ngOnInit(): void {
+    this.detectColorScheme();
+  }
+
+  detectColorScheme() {
+    const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+    if (prefersDarkScheme) {
+      this.renderer.setAttribute(document.body, 'data-theme', 'dark');
+    } else {
+      this.renderer.setAttribute(document.body, 'data-theme', 'light');
+    }
+
+    // Listener für Änderungen des Farbschemas
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
+      const newColorScheme = e.matches ? 'dark' : 'light';
+      this.renderer.setAttribute(document.body, 'data-theme', newColorScheme);
+    });
+  }
 }
