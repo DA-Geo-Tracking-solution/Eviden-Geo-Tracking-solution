501e28e56f3c5d85116be281d13b72f7710f25aa
Benjamin Suljevic <bsuljevic1809@gmail.com>
2024-10-19 17:46:55 +0200

    start with the CreateUserView

--- Frontend/geo-tracking-solution/src/app/app-routing.module.ts
+++ Frontend/geo-tracking-solution/src/app/app-routing.module.ts
@@ -5,10 +5,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { authGuard } from './services/guard/auth.guard';
+import { CreateUserComponent } from './components/create-user/create-user.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, canActivate:  [ authGuard ]},
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  {path:'createUser', component: CreateUserComponent}
 ];
 
 @NgModule({
---
+++ Frontend/geo-tracking-solution/src/app/components/create-user/create-user.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-user',
+  templateUrl: './create-user.component.html',
+  styleUrl: './create-user.component.css'
+})
+export class CreateUserComponent {
+
+}
