38e9eddb78130a23afb4696d2887b64208bb7946
Felix <felix.gregor.ebertz@gmail.com>
2025-01-24 10:44:38 +0100

    RabbitMQStructure updated

--- Backend/rabbitmq/rabbitmqreceiver/wait-for-cassandra.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-until cqlsh -e "describe keyspaces"; do
-  echo "Cassandra is unavailable - waiting"
-  sleep 2
-done
-
-echo "Cassandra is up - executing CQL script"
-cqlsh -f /docker-entrypoint-initdb.d/init.cql
